ba8dbd08bbbe2fd39813c85b7b05f308cd497c04
Navin Chandrakar <88479575+navinfc@users.noreply.github.com>
2024-10-01 12:05:18 +0530

    created frontend

---
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { BiLogOutCircle } from "react-icons/bi";
+
+const LogoutButton = () => {
+  return (
+    <div className='mt-auto'>
+      <BiLogOutCircle className='w-6 h-6 text-white cursor-pointer'/>
+    </div>
+  )
+}
+
+export default LogoutButton
